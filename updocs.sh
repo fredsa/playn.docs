551be6a6d8749b98428279da3dad4f282496c1e9
Michael Bayne <mdb@samskivert.com>
2012-07-11 15:54:41 -0700

    Made updocs easier to steal for other projects.

--- updocs.sh
+++ updocs.sh
@@ -1,21 +1,24 @@
 #!/bin/sh
 
+GROUP=com/googlecode/playn
+ARTIFACT=playn-core
+
 if [ -z "$1" ]; then
     echo "Usage: $0 M.N"
     echo "Where M.N is the version of the just performed release."
     exit 255
 fi
 
-COREDIR=$HOME/.m2/repository/com/googlecode/playn/playn-core/$1
+COREDIR=$HOME/.m2/repository/$GROUP/$ARTIFACT/$1
 if [ ! -d $COREDIR ]; then
     echo "Can't find: $COREDIR"
     echo "Is $1 the correct version?"
     exit 255
 fi
 
-echo "Unpacking playn-core-$1-javadoc.jar..."
+echo "Unpacking $ARTIFACT-$1-javadoc.jar..."
 cd javadoc
-jar xf $COREDIR/playn-core-$1-javadoc.jar
+jar xf $COREDIR/$ARTIFACT-$1-javadoc.jar
 rm -rf META-INF
 
 echo "Adding and committing updated docs..."
